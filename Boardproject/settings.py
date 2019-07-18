641bdba45b6eb2cda6f1042e9608f322aeab6b2a
LEE <invisibleWarehouse>
2019-07-19 02:22:35 +0900

    deploy

--- Boardproject/settings.py
+++ Boardproject/settings.py
@@ -27,11 +27,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'login.apps.LoginConfig',
     'Boardapp.apps.BoardappConfig',
